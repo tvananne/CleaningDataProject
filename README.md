b29dfd24cf5da69f970071cc7c04dcc53b9563d8
Taylor Van Anne <tvananne@textron.com>
2015-12-12 12:17:37 -0600

    Signed-off-by: Taylor Van Anne <tvananne@textron.com>

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # CleaningDataProject
-Getting and Cleaning Data Course Project
-This is the thing
+http://www.unexpected-vortices.com/sw/rippledoc/quick-markdown-example.html (good example for how markdown works) \n \n
+Getting and Cleaning Data Course Project \n \n
+This script reads in data from a directory (UCI HAR Dataset) and puts that data in a "tidy" format.
 
-## Running the script
+## How To:
 
-### 1) set the location of your data
+### 1) Set the location of your data
 First things first, find this line of code in my script and make sure it points to the your UCI HAR Dataset file (make sure to include the '/' at the end so the code that references this string works properly).
 
        file.loc <- '~/Analytics/Coursera/03 Getting and Cleaning Data/CourseProjData/UCI HAR Dataset/'
 
-### 2) everything else should be good lol
+### 2) Execute the run_analysis.R script
+
+After the file.loc script variable has been changed to your data directory, the script can be executed.
+
+### 3) Output 
+
+The run_analysis.R script generates the CleanAndTidy.csv and CleanAndTidy.txt files
+
+## Codebook
+
+Please see the code book in this repo for more information regarding the variables in the output files and 
+
--- run_analysis.R
+++ run_analysis.R
@@ -8,23 +8,24 @@ library(data.table)
 library(dplyr)
 
 
-# Initialize script variables *********************************************************************************** 
+# Initialize script variables *************************************************************************** 
        library(data.table)
-       file.loc <- '~/Analytics/Coursera/03 Getting and Cleaning Data/CourseProjData/UCI HAR Dataset/' #set your location for data files
+       #set your location for the data files here in file.loc
+       file.loc <- '~/Analytics/Coursera/03 Getting and Cleaning Data/CourseProjData/UCI HAR Dataset/' 
        activity_labels <- fread(paste(file.loc, "activity_labels.txt", sep = ""))
        activityNumber <- 'actyNumb'
        subjectNumber <- 'subjectNumber'
        feature <- fread(paste(file.loc, "features.txt", sep = ""))
        feature_labels <- feature$V2; rm(feature)       
 
-# Combine TEST data ********************************************************************************************
+# Combine TEST data into one file ***********************************************************************
        X_test <- fread(paste(file.loc, "test/X_test.txt", sep = ""))
        colnames(X_test) <- feature_labels
        
        test_subjects <- fread(paste(file.loc, "test/subject_test.txt", sep = ""))
        colnames(test_subjects) <- subjectNumber
        
-       Y_test <- fread("~/Analytics/Coursera/03 Getting and Cleaning Data/CourseProjData/UCI HAR Dataset/test/y_test.txt")
+       Y_test <- fread(paste(file.loc, "test/y_test.txt", sep = ""))
        colnames(activity_labels) <- c(activityNumber, 'actyLabel')
        colnames(Y_test) <- activityNumber
        test <- cbind(X_test, test_subjects, Y_test) #put the activity type label at the end of each observation
@@ -38,15 +39,15 @@ library(dplyr)
        
        rm(test)      #keep global environment clean
        
-# Combine TRAIN data *******************************************************************************************
-       X_train <- fread("~/Analytics/Coursera/03 Getting and Cleaning Data/CourseProjData/UCI HAR Dataset/train/X_train.txt")
+# Combine TRAIN data into one file **********************************************************************
+       X_train <- fread(paste(file.loc, "train/X_train.txt", sep = ""))
        colnames(X_train) <- feature_labels
        
        train_subjects <- fread(paste(file.loc, "train/subject_train.txt", sep = ""))
        colnames(train_subjects) <- subjectNumber
        
        
-       Y_train <- fread("~/Analytics/Coursera/03 Getting and Cleaning Data/CourseProjData/UCI HAR Dataset/train/y_train.txt")
+       Y_train <- fread(paste(file.loc, "train/y_train.txt", sep = ""))
        colnames(activity_labels) <- c(activityNumber, 'actyLabel')
        colnames(Y_train) <- activityNumber
        
@@ -66,8 +67,9 @@ library(dplyr)
                      unique(new_train$subjectNumber)
                      unique(new_test$subjectNumber)
        
-# Combine TEST and TRAIN into combined *****************************************************************************************************************     
+# Combine TEST and TRAIN into 'combined' data set ***************************************************    
        
+       #need both data sets in a list for combining data.table
        l = list(new_test, new_train)
        
        #this is the combined (train and test) data table
@@ -81,7 +83,8 @@ library(dplyr)
                      print("Something went wrong! 'combined' data.table has a length of 0")
               }
        
-       
+
+# This block builds the actual tidy data set we are after ******************************************
        feature_labels <- c(feature_labels, subjectNumber, activityNumber, 'actyLabel')
        rm(train, activityNumber)
        
@@ -100,7 +103,7 @@ library(dplyr)
        combinedDF <- as.data.frame(combined)     #this type of subset doesn't work on DT, only DF
        meanAndStd <- (combinedDF[extraction])
        class(meanAndStd); rm(combinedDF)         #meanAndStd is a DF of the features we want, remove combinedDF
-    
+
        #meanAndStd is our data.frame with mean / standard deviation and the activity number / label
        meanAndStd <- as.data.table(meanAndStd)
 
@@ -136,6 +139,7 @@ library(dplyr)
        subDT.grp <- group_by(subDT, subjectNumber, actyLabel) 
        
        #has to be a better way, but this function took 10 seconds to produce in excel
+       #see 'FormulaHelper.xlsx' file in this repo for a reference
        subDT.summ <- summarize(subDT.grp, `1` = mean(`1`),
                                `2` = mean(`2`),
                                `3` = mean(`3`),
@@ -216,12 +220,10 @@ library(dplyr)
                                `78` = mean(`78`),
                                `79` = mean(`79`))
        
-       
-       
        colnames(subDT.summ) <- newColNames
        
        write.csv(subDT.summ, file = "CleanAndTidy.csv")
        write.table(subDT.summ, file = "CleanAndTidy.txt", row.names = FALSE)
        
-      
+      rm(activity_labels, combined, subDF, subDT, subDT.grp, test_subjects, train_subjects)
        
